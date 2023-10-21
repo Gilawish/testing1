5fd88d2ce6d1dcd2f12ed5ecca47e1a0c3e775d9
Gilawish <gilawishbaig2004@gmail.com>
2023-10-21 16:59:44 -0700

    assnmnt 11 added

---
+++ app.js
@@ -0,0 +1,102 @@
+//Q NO 1
+var country = prompt("Please enter your country:");
+var city = prompt("Please enter your city:");
+var area = prompt("Please enter your area:");
+
+if (country.toLowerCase() === "pakistan" && city.toLowerCase() === "karachi" && area.toLowerCase() === "defence") {
+    console.log("Welcome.");
+} else {
+    console.log("Invalid area.");
+}
+                     //Q NO 2//
+ 
+//var userPreference = prompt("Do you prefer tea or coffee?");
+//userPreference = userPreference.toLowerCase();
+//if (userPreference === "tea") {
+    //alert("Here is your tea.");
+//} else if (userPreference === "coffee") {
+   // alert("Here is your coffee.");}
+ //else {
+    //alert("Something went wrong.");}
+
+                      //Q NO 3
+
+//var hasMembership = prompt("Do you have a membership? (Yes / No)").toLowerCase();
+//var isStudent = prompt("Are you a student? (Yes / No)").toLowerCase();
+//if (hasMembership === "yes" || isStudent === "yes") {
+    //alert("You qualified for a discount.");}
+ //else 
+    //alert("Sorry, no discount for you.");}
+
+                 // Q NO 4
+
+   // var userAge = parseInt(prompt("Please enter your age:"));
+//var hasValidID = prompt("Do you have a valid ID? (Yes / No)").toLowerCase();
+//if (userAge > 18 && hasValidID === "yes") {
+    //alert("You are eligible.");} else {
+   // alert("Sorry, you are not eligible.");}
+
+                 //Q NO 5
+
+   //var fatherIsPaying = (prompt("Father is paying ")); 
+//var isSale = (prompt(" there's a sale?"));
+//if (fatherIsPaying  === "yes" || isSale === "yes") {
+ //console.log("You will go shopping.");}
+ //else {
+   //console.log("You will not go shopping.");}
+
+                    //Q no 6
+ 
+//var userAge = parseInt(prompt("Please enter your age:"));
+
+//var ownsCar = prompt("Do you own a car? (Yes / No)").toLowerCase();
+
+//if (userAge > 17 && ownsCar === "yes") {
+   // console.log("You can drive and you own a car.");} 
+    //else {
+   // console.log("You cannot drive a car.");}
+
+
+              //Q NO 7
+
+//var dayNumber = parseInt(prompt("Enter a number (1-7) for the day of the week:"));
+//if (dayNumber === 1) {
+    //console.log("It's Monday.");} else if (dayNumber === 2) {
+    //console.log("It's Tuesday.");} else if (dayNumber === 3) {
+    //console.log("It's Wednesday.");} else if (dayNumber === 4) {
+    //console.log("It's Thursday.");} else if (dayNumber === 5) {
+    //console.log("It's Friday.");} else if (dayNumber === 6) {
+    //console.log("It's Saturday.");} else if (dayNumber === 7) {
+    //console.log("It's Sunday.");}
+    // else {
+   // console.log("Invalid input. Please enter a number between 1 and 7.");}
+
+
+                        //Q NO 8
+
+//var userChoice = prompt("Choose a traffic light color (Red, Yellow, Green):").toLowerCase();
+//if (userChoice === "red") {
+    //console.log("Stop! The light is red.");} else if (userChoice === "yellow") {
+    //console.log("Proceed with caution. The light is yellow.");} else if (userChoice === "green") {
+   // console.log("Go ahead. The light is green.");} else {
+   // console.log("Invalid input. Please choose Red, Yellow, or Green.");}
+  
+                             //Q NO 9
+   
+   //var userDay = prompt("Enter a day of the week:").toLowerCase();
+//if (userDay === "monday" || userDay === "tuesday" || userDay === "wednesday" || userDay === "thursday" || userDay === "friday") {
+    //console.log("It's a weekday.");} else if (userDay === "saturday" || userDay === "sunday") {
+    //console.log("It's a weekend day.");} else {
+    //console.log("Invalid input. Please enter a valid day of the week.");}
+
+                             // Q NO 10
+
+//var age = parseInt(prompt("Enter your age:"));
+//if (age < 0) {
+    //console.log("Invalid age input.");} else if (age <= 12) {
+    //console.log("You are a child.");} else if (age <= 19) {
+    //console.log("You are a teenager.");} else if (age <= 64) {
+    //console.log("You are an adult.");} else {
+   // console.log("You are a senior.");}
+
+
